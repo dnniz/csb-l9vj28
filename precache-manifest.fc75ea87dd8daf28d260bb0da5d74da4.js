f5001c0a28a0df3667fcde02442c443c29bc258a
Code Sandbox <hello@codesandbox.io>
2023-08-15 02:30:45 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.fc75ea87dd8daf28d260bb0da5d74da4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e9b588c5b3f88023955",
+    "url": "https://dnniz.github.io/csb-l9vj28/static/js/main.460488a2.chunk.js"
+  },
+  {
+    "revision": "acfe8c5f4dd17224423d",
+    "url": "https://dnniz.github.io/csb-l9vj28/static/js/runtime~main.2de41dba.js"
+  },
+  {
+    "revision": "cd176291fbc429c8353f",
+    "url": "https://dnniz.github.io/csb-l9vj28/static/js/2.aa5b83c0.chunk.js"
+  },
+  {
+    "revision": "7b906b526c4ce1c6d1be510cc3908bf2",
+    "url": "https://dnniz.github.io/csb-l9vj28/index.html"
+  }
+];
